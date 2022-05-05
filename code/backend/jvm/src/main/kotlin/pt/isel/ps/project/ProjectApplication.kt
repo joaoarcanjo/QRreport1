5653f935c58b8be931d7ae8408f99ab7d55eeb34
Diogo Novo <diogo.a.novo@gmail.com>
2022-05-05 15:34:18 +0100

    Initial Spring project.

---
+++ code/backend/jvm/src/main/kotlin/pt/isel/ps/project/ProjectApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.ps.project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectApplication>(*args)
+}
---
+++ code/backend/jvm/src/test/kotlin/pt/isel/ps/project/ProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.isel.ps.project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
