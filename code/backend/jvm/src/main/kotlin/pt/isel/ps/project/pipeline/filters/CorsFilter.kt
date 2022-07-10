e94ae45325e4f23fa71effd31223be83da979b02
Diogo Novo <diogo.a.novo@gmail.com>
2022-07-10 11:21:20 +0100

    Fixed bug on CorsFilter.

--- code/backend/jvm/src/main/kotlin/pt/isel/ps/project/pipeline/filters/CorsFilter.kt
+++ code/backend/jvm/src/main/kotlin/pt/isel/ps/project/pipeline/filters/CorsFilter.kt
@@ -12,20 +12,21 @@ class CorsFilter : OncePerRequestFilter() {
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
         // In case there isn't a preflight request, continue...
         if (request.method != "OPTIONS") filterChain.doFilter(request, response)
-
-        response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000")
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD")
-        response.addHeader(
-            "Access-Control-Allow-Headers",
-            "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, " +
-                    "Access-Control-Request-Headers, $ORIGIN_HEADER"
-        )
-        response.addHeader(
-            "Access-Control-Expose-Headers",
-            "Access-Control-Allow-Origin, Access-Control-Allow-Credentials"
-        )
-        response.addHeader("Access-Control-Allow-Credentials", "true")
-        response.addIntHeader("Access-Control-Max-Age", 60)
-        response.status = 200
+        else {
+            response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD")
+            response.addHeader(
+                "Access-Control-Allow-Headers",
+                "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, " +
+                        "Access-Control-Request-Headers, $ORIGIN_HEADER"
+            )
+            response.addHeader(
+                "Access-Control-Expose-Headers",
+                "Access-Control-Allow-Origin, Access-Control-Allow-Credentials"
+            )
+            response.addHeader("Access-Control-Allow-Credentials", "true")
+            response.addIntHeader("Access-Control-Max-Age", 60)
+            response.status = 200
+        }
     }
 }
